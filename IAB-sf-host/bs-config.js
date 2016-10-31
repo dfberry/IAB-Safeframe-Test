b0d6687aef1f135551160a870de3e39e5e970098
dfberry <dfberry@admins-MacBook-Air.local>
2016-10-31 09:42:18 -0700

    bug fixes for docker setup

--- IAB-sf-host/bs-config.js
+++ IAB-sf-host/bs-config.js
@@ -2,5 +2,5 @@ module.exports = {
   "logLevel": "debug",
   "port": 8010,
   "files": ["./website/**/*.{html,htm,css,js}"],
-  "server": { "baseDir": "./ads" }
+  "server": { "baseDir": "./website" }
 };
